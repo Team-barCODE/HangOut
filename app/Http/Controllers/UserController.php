bf1046b0a0c211e83532ebebfb3897a313f98a39
ruderude <rude1979@gmail.com>
2020-05-16 14:53:38 +0900

    修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,6 +35,7 @@ class UserController extends Controller
     public function index(int $status = 0)
     {
         // dd($this->service->getNum());
+        $authUser = Auth::user();
         $users = $this->service->getList($status);
 
         // 表示テスト用
