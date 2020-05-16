10d74f300354bac0420913870adca9d2698a78fd
sunsuke <nisemono12041141@gmail.com>
2020-05-17 02:51:53 +0900

    グローバルナビ修正(ipadの768pxを起点にGnaviのデフォルト表示切替) _userlist.scssの一覧画像再調整(max-width指定だとサイズ感がばらけるのでデフォルトをvmin指定に変更)

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
         @if((Auth::id() !== null))
 
             <li class="col-5 mb-2 mt-2 text-center">
-                <a href="/users/show/{{Auth::id()}}">
+                <a href="/users/show/{{Auth::id()}}" class="text-white">
                     <i class="fas fa-user fa-2x"></i>
                 </a>
             </li>
@@ -32,27 +32,25 @@
         @endif
     </ul>
     <nav class="gnavi-contents m-0">
-      <ul class="row align-items-center justify-content-around p-0 m-0">
-          <li class="list_none mt-2 mb-2 col-md-2">
-              <a class="btn btn-sm btn-primary d-block" href="/users/show/{{Auth::id()}}">マイページ</a>
-          </li>
-          <li class="list_none mt-2 mb-2 col-md-2">
-              <a class="btn btn-sm btn-primary d-block"  href="{{ route('home') }}">スワイプ</a>
-          </li>
-          <li class="list_none mt-2 mb-2 col-md-2">
-              <a class="btn btn-sm btn-primary d-block" href="{{ route('users.index') }}">一覧</a>
-          </li>
-          <li class="list_none mt-2 mb-2 col-md-2">
-              <a class="btn btn-sm btn-primary d-block" href="{{ route('matching') }}">チャット</a>
-          </li>
-          <li class="list_none mt-2 mb-2 col-md-2">
-              <a class="btn btn-sm btn-primary d-block" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">ログアウト</a>
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                  @csrf
-              </form>
-          </li>
-      </ul>
+        <ul class="row align-items-center justify-content-around p-0 m-0">
+            <li class="list_none mt-2 mb-2 col-md-2">
+                <a class="btn btn-sm bg-white d-block font-weight-bold" href="/users/show/{{Auth::id()}}">マイページ</a>
+            </li>
+            <li class="list_none mt-2 mb-2 col-md-2">
+                <a class="btn btn-sm bg-white d-block font-weight-bold"  href="{{ route('home') }}">スワイプ</a>
+            </li>
+            <li class="list_none mt-2 mb-2 col-md-2">
+                <a class="btn btn-sm bg-white d-block font-weight-bold" href="{{ route('users.index') }}">一覧</a>
+            </li>
+            <li class="list_none mt-2 mb-2 col-md-2">
+                <a class="btn btn-sm bg-white d-block font-weight-bold" href="{{ route('matching') }}">チャット</a>
+            </li>
+            <li class="list_none mt-2 mb-2 col-md-2">
+                <a class="btn btn-sm bg-white d-block font-weight-bold" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">ログアウト</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+        </ul>
     </nav>
-
-
 </header>
