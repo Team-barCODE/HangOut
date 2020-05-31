0172256ca8f8e54866a99b9916fe8c1128b9d9d4
sunsuke <nisemono12041141@gmail.com>
2020-05-31 18:35:34 +0900

    test

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,30 +1,21 @@
-@extends('layouts.layout')
+@extends('adminlte::page')
+
+@section('title', '管理画面')
+@section('dashboard_url', route('admin.home'))
+@section('dashboard_url', route('admin.logout'))
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
 @section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
 
-<div class="topPage">
-  <div id="tinderslide">
-    <ul>
-        @foreach($users as $user)
-        <li data-user_id="{{ $user->id }}">
-          <div class="userName">{{ $user->name }}</div>
-          <img src="/storage/images/{{ $user->img_name1}}">
-          <div class="like"></div>
-          <div class="dislike"></div>
-        </li>
-        @endforeach
-    </ul>
-    <div class="noUser">近くにお相手がいません。</div>
-  </div>
-  <div class="actions" id="actionBtnArea">
-      <a href="#" class="dislike"><i class="fas fa-times fa-2x"></i></a>
-      <a href="#" class="like"><i class="fas fa-heart fa-2x"></i></a>
-  </div>
-</div>
+@section('css')
+    <link rel="stylesheet" href="admin-lte/css/admin_custom.css">
+@stop
 
-<script>
-  var usersNum = {{ $userCount }};
-  var from_user_id = {{ $from_user_id }};
-</script>
-
-@endsection
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
